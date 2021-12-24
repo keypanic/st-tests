52b1fc0b1e91d938c629235e9f1bae3ade78bc09
keypanic <kaigauscom@gmail.com>
2021-12-25 01:48:08 +0300

    feat: generate random_arrays/zip_function test data, save to .json

--- task4_test/index.js
+++ task4_test/index.js
@@ -1,15 +1,26 @@
 const fs = require('fs');
 const { generate_arrays } = require('./src/util');
+const _ = require('lodash');
 
-let d1 = generate_arrays(100, 1);
-let d2 = generate_arrays(100, 2);
-let data = [...d1, ...d2].map(el => {
+const d1 = generate_arrays(150, 1);
+const d2 = generate_arrays(150, 2);
+
+const arrays = [...d1, ...d2].map(el => {
   let n = Math.round(Math.random() * el.length);
+  if(Math.random() < .1) n = -n;
   return { array: el, n: n };
 });
 
-fs.writeFile('./test_data/array_data.json', JSON.stringify(data), 'utf-8', (err) => {
-  if(err) console.log(err);
-  else console.log('json saved');
-})
- 
+const zip_data = arrays.map(obj => {
+  return _.chunk(obj.array, obj.n)
+});
+
+function write(path, data) {
+  fs.writeFile(path, JSON.stringify(data), 'utf-8', (err) => {
+    if(err) console.log(err);
+    else console.log('json saved');
+  });
+}
+
+write('./test_data/arrays.json', arrays);
+write('./test_data/zip_data.json', zip_data);
